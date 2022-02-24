f976e9ff725aa9fbd60d2145bf16e3c475875959
tfkhdyt <ta142fa@gmail.com>
2022-02-24 10:30:18 +0700

    format & lint

--- components/angkaBesarKecil/Header.tsx
+++ components/angkaBesarKecil/Header.tsx
@@ -1,6 +1,8 @@
 const Header = () => {
   return (
-    <h1 className='text-xl font-bold text-slate-800'>Angka Lebih Besar atau Lebih Kecil</h1>
+    <h1 className='text-xl font-bold text-slate-800'>
+      Angka Lebih Besar atau Lebih Kecil
+    </h1>
   )
 }
 
--- pages/redux-toolkit/index.tsx
+++ pages/redux-toolkit/index.tsx
@@ -1,7 +1,11 @@
-import {FormEvent, useState} from "react"
-import {useDispatch, useSelector} from "react-redux"
-import { AppDispatch, RootState } from "../../store"
-import { decrement, increment, incrementByAmount } from '../../store/counterSlice'
+import { FormEvent, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../store'
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from '../../store/counterSlice'
 
 interface CardProps {
   title: string
@@ -12,30 +16,44 @@ interface CardProps {
 const Card = ({ title, count, dispatch }: CardProps) => {
   const [custom, setCustom] = useState<number>(0)
   return (
-    <div className="card w-full md:w-96 bg-base-100 shadow-xl mx-auto">
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <div className='flex flex-col justify-center mb-2'>
-          <p className='font-semibold text-lg text-center'>Counter</p>
-          <p className='text-[5rem] font-black text-center'>
-            {count}
-          </p>
+    <div className='card mx-auto w-full bg-base-100 shadow-xl md:w-96'>
+      <div className='card-body'>
+        <h2 className='card-title'>{title}</h2>
+        <div className='mb-2 flex flex-col justify-center'>
+          <p className='text-center text-lg font-semibold'>Counter</p>
+          <p className='text-center text-[5rem] font-black'>{count}</p>
         </div>
         <div className='flex space-x-2'>
-          <input type="text" placeholder="Type here" className="input input-bordered w-8/12 max-w-xs" onChange={(e: FormEvent<HTMLInputElement>) => setCustom(Number(e.currentTarget.value))}/>
-          <button className="btn btn-accent w-4/12" onClick={() => dispatch(incrementByAmount(custom))}>Set</button> 
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input-bordered input w-8/12 max-w-xs'
+            onChange={(e: FormEvent<HTMLInputElement>) =>
+              setCustom(Number(e.currentTarget.value))
+            }
+          />
+          <button
+            className='btn btn-accent w-4/12'
+            onClick={() => dispatch(incrementByAmount(custom))}
+          >
+            Set
+          </button>
         </div>
-        <div className="justify-center card-actions flex">
-          <button 
-            className="btn btn-primary" 
+        <div className='card-actions flex justify-center'>
+          <button
+            className='btn btn-primary'
             onClick={() => dispatch(increment())}
-            disabled={(count <= 5) ? false : true}
-          >Increment</button>
-          <button 
-            className="btn btn-secondary" 
+            disabled={count <= 5 ? false : true}
+          >
+            Increment
+          </button>
+          <button
+            className='btn btn-secondary'
             onClick={() => dispatch(decrement())}
-            disabled={(count == 0) ? true : false}
-          >Decrement</button>
+            disabled={count == 0 ? true : false}
+          >
+            Decrement
+          </button>
         </div>
       </div>
     </div>
@@ -47,7 +65,7 @@ const ReactToolkit = () => {
   const dispatch = useDispatch()
   return (
     <div className='container mx-auto p-4'>
-      <Card title='Redux Toolkit Counter' count={count} dispatch={dispatch}/>
+      <Card title='Redux Toolkit Counter' count={count} dispatch={dispatch} />
     </div>
   )
 }
